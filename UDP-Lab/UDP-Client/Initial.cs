b9c96d65fc2d693d26d8d62668d6f4663c512fba
Alexresh <alexresh62@yandex.ru>
2020-09-19 01:43:41 +0300

    Added client and server Added 1 question without answer

---
+++ UDP-Lab/UDP-Client/Initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_Client
+{
+    class Initial
+    {
+        static void Main(string[] args)
+        {
+
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5002);
+
+
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("1");
+
+                client.Send(bytes, bytes.Length, endPoint);
+                IPEndPoint server = null;
+                byte[] receiveBytes = client.Receive(ref server);
+                Console.WriteLine(Encoding.UTF8.GetString(receiveBytes));
+                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
+            }
+            finally 
+            { 
+                client.Close();
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ UDP-Lab/UDP-Server/Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_Server
+{
+    class Initial
+    {
+        private static List<IPEndPoint> clients = new List<IPEndPoint>();
+        static void Main(string[] args)
+        {
+            UdpClient server = new UdpClient(5002);
+
+            
+            server.Client.ReceiveTimeout = 1000 * 10;
+
+            try
+            {
+                IPEndPoint RemoteIpEndPoint = null;
+                while (true) 
+                {
+                    byte[] receiveBytes = server.Receive(ref RemoteIpEndPoint);
+                    clients.Add(RemoteIpEndPoint);
+                }
+                //string returnData = Encoding.UTF8.GetString(receiveBytes);
+            }
+            catch (SocketException)
+            {
+                
+            }
+            byte[] firstQuestion = Encoding.UTF8.GetBytes("Было 3 козла.\nСколько?");
+            clients.ForEach(client=> { server.Send(firstQuestion, firstQuestion.Length, client); });
+            Console.ReadLine();
+        }
+
+    }
+}
